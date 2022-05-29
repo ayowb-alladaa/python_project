5139ebcfc3f4ec41940f2928ab301a5f0405beeb
PC <ayowb246@gmail.com>
2022-05-29 19:34:41 +0300

    fff

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ class Client(Person):
         self.phone_number = phone_number
 
     def print(self):
-        print(self.id, self.full_name, self.age, self.id_on,self.phone_number)
+        print("id=>",self.id," full_nume=>", self.full_name," age=>", self.age," id_on=>", self.id_on," phone_number=>",self.phone_number)
 ####################
 class Librarian(Person):
     def __init__(self, id, full_name, age, id_on,emplyment_type):
@@ -19,16 +19,16 @@ class Librarian(Person):
         self.emplyment_type = emplyment_type
 
     def print(self):
-        print(self.id, self.full_name, self.age, self.id_on, self.emplyment_type)
+        print("id=>",self.id," full_nume=>", self.full_name," age=>", self.age," id_on=>", self.id_on," emplyment_type=>",self.emplyment_type)
 
 ####################
 class Book:
-  def __init__(self, id, title,description,author):
+  def __init__(self, id, title,description,author,status):
     self.id = id
     self.title = title
     self.description=description
     self.author=author
-    self.status ="Activ"
+    self.status =status
 
 
   def reservation(self):
@@ -46,70 +46,125 @@ class Book:
           print("not succeeded")
 
   def print(self):
-        print(self.id, self.title, self.description, self.author, self.status)
+        print("id=>",self.id," title=>", self.title, " description=>",self.description," author=>", self.author," status"," status=>", self.status)
 ####################
-class Borrowing_Order:
-  def __init__(self, id, date,client_id,book_id,status):
-    self.id = id
-    self.date = date
-    self.client_id=client_id
-    self.book_id=book_id
-    self.status =status
-########################3
-c=Client(101,"Ayoub",20,999,59233)
+
+c=Client("101","Ayoub",20,999,59233)
 
 l=Librarian(102,"diaa",21,888,"Full")
 
-b=Book(103,"Python","hhgfh bv","DiaaSamy")
-b2=Book(104,"java","blabla","DiaaSamy")
-b3=Book(105,"dart","cont","DiaaSamy")
 
 my_objects = []
-my_objects.append(Book(103,"Python","hhgfh bv","DiaaSamy"))
-my_objects.append(Book(104,"java","blabla","DiaaSamy"))
-my_objects.append(Book(105,"dart","cont","DiaaSamy"))
+my_objects.append(Book("103","Python","hhgfh bv","DiaaSamy","Activ"))
+my_objects.append(Book("104","java","blabla","DiaaSamy","Activ"))
+my_objects.append(Book("105","dart","cont","DiaaSamy","InActiv"))
 
 a=0
-print("1-List of clintes\n2-List of librarians\n3-List of Books\n4-List of borrowed_orders\n5-total_borrowed_books\n6-total_available_books\n7-total_borrowed_orders\n8-Exit")
+aa=0
+print("________________________________________________")
+print("1-List of clintes:")
+c.print()
+print("________________________________________________")
+print("2-List of librarians:")
+l.print()
+print("________________________________________________")
+print("3-List of Books:")
+for obj in my_objects:
+    obj.print()
+print("________________________________________________")
+print("4-total_borrowed_books:")
+for obj in my_objects:
+   if obj.status == "InActiv":
+       a += 1
+   else:
+       aa+=1
+
+print(a,"inavailable books")
+a=0
+print("________________________________________________")
+print("5-total_available_books:")
+print(aa, "available books")
+aa=0
+print("________________________________________________")
+print("Welcome to the library")
+print("1-borrow book now")
+print("2-Cancel a book reservation now")
+print("*-exit")
 op=input()
-if op=="1":
+while op=="1"or op=="2" :
+    if op == "1":
+        print("Enter the book id:")
+        book_id = input()
+
+        for obj in my_objects:
+            if obj.id == book_id:
+                if obj.status == "Activ":
+                    print("Enter your ID")
+                    id = input()
+                    if c.id == id:
+                        obj.reservation()
+                        obj.print()
+
+                else:
+                    print("book is booked")
+                f = True
+                break
+            else:
+                f = False
+
+        if f == False:
+            print("not find")
+
+    elif op == "2":
+        print("Enter the book id:")
+        book_id = input()
+        for obj in my_objects:
+            if obj.id == book_id:
+                if obj.status == "InActiv":
+                    print("Enter your ID")
+                    id = input()
+                    if c.id == id:
+                        obj.cancl_reservation()
+                        obj.print()
+
+
+
+    else:
+        exit()
+    print("________________________________________________")
+    print("1-List of clintes:")
     c.print()
-elif op=="2":
+    print("________________________________________________")
+    print("2-List of librarians:")
     l.print()
-elif op=="3":
+    print("________________________________________________")
+    print("3-List of Books:")
     for obj in my_objects:
         obj.print()
-elif op=="4":
-   print()
-elif op=="5":
-    print()
-elif op=="6":
+    print("________________________________________________")
+    print("4-total_borrowed_books:")
     for obj in my_objects:
-        obj.status=="Activ"
-        a+=1
-    print(a ,"available books")
+        if obj.status == "InActiv":
+            a += 1
+        else:
+            aa += 1
+
+    print(a, "inavailable books")
+    a = 0
+    print("________________________________________________")
+    print("5-total_available_books:")
+    print(aa, "available books")
+    aa = 0
+    print("________________________________________________")
+    print("1-borrow book now")
+    print("2-Cancel a book reservation now")
+    print("*-exit")
+    op=input()
 
 
-elif op=="7":
-    print()
-else :
-    exit
 
 
 
-
-
-
-
-
-
-
-
-# b.print()
-# b.reservation()
-# b.print()
-# b.cancl_reservation()
-# b.print()
 
 
 
